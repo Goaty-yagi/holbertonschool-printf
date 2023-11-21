804777defa20710cfaa6d64b5d15a63cd8ad432f
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2023-11-21 17:05:59 +1100

    update main.h

--- main.h
+++ main.h
@@ -3,7 +3,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h> //this is not working//
+#include <stdlib.h>
 int _printf(const char *format, ...);
 int _putchar(char c);
 
